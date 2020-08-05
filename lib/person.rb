4a5e821e35db16a4e7bb880b9f29123b428ce37c
gregaswright <greg.a.s.wright@gmail.com>
2020-08-05 11:23:42 -0400

    gregs commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,41 @@
-# your code goes here
+class Person
+
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value > 10
+            value = 10
+        elsif value < 0
+            value = 0
+        end
+        @happiness = value
+    end
+
+    def hygiene=(value)
+        if value > 10
+            value = 10
+        elsif value < 0
+            value = 0
+        end
+        @hygiene = value
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+    
+    def happy?
+         self.happiness > 7
+    end
+    
+
+end
