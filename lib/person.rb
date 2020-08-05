9e65b01afe8a35a684add8dcd709931be16c56f9
AmSmo <adam.smolenski@gmail.com>
2020-08-05 11:32:07 -0400

    Hooraygit add .

--- lib/person.rb
+++ lib/person.rb
@@ -32,6 +32,52 @@ class Person
             return num
          end
     end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            return "blah blah partisan blah lobbyist" 
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            return "blah blah sun blah rain" 
+        else
+            return 'blah blah blah blah blah'
+        end
+
+    
+    end
 end
 #binding.pry
 
