fcb997ee54b92348e241c6c9f68f04bc90dcbfbd
slurio <samantha.lurio@gmail.com>
2020-08-05 11:45:26 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -39,17 +39,17 @@ class Person
         "all about the benjamins"
     end
     def take_bath
-        self.hygiene=(@hygiene + 4)
+        self.hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
     def work_out
-        self.hygiene=(@hygiene-3)
-        self.happiness=(@happiness+2)
+        self.hygiene -= 3
+        self.happiness += 2
         "♪ another one bites the dust ♫"
     end
     def call_friend(friend)
-        self.happiness=(@happiness+3)
-        friend.happiness=(friend.happiness+3)
+        self.happiness += 3
+        friend.happiness += 3
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
     def start_conversation(friend, topic)
