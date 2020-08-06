d96e6c5d97b9ffdd425b53daa7525d3e6b9ab18f
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-05 20:04:40 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,16 @@
 require 'pry'
-​
+
 class Person
     attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
-​
+
     def initialize (name)
         @name = name
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
     end
-​
+
     def happiness=(score)
         if score >= 0 && score <= 10
             @happiness = score
@@ -19,9 +19,9 @@ class Person
         else
             @happiness = 10
         end
-​
+
     end
-​
+
     def hygiene=(score)
         if score >= 0 && score <= 10
             @hygiene = score
@@ -31,48 +31,48 @@ class Person
             @hygiene = 10
         end
     end
-​
+
     def clean?
         @hygiene >7 ? true :false
     end
-​
+
     def happy?
         @happiness >7 ? true :false
     end
-​
+
     def get_paid (salary)
         @bank_account += salary
         "all about the benjamins"
     end
-​
+
     def take_bath
         
         self.hygiene = @hygiene + 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
-​
+
     def work_out
         self.hygiene = @hygiene -3
         self.happiness = @happiness +2
         "♪ another one bites the dust ♫"
     end
-​
+
     #
     def call_friend(friend)
         self.happiness = @happiness + 3
         friend.happiness = friend.happiness + 3
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
-​
+
     def start_conversation (friend, topic)
         case topic
-​
+
         when 'politics'
         #dosomething
             self.happiness = @happiness  - 2
             friend.happiness = friend.happiness - 2
             return 'blah blah partisan blah lobbyist' 
-​
+
         when 'weather'
             #binding.pry
             self.happiness = @happiness + 1
@@ -82,6 +82,6 @@ class Person
             return 'blah blah blah blah blah'
         end
     end
-​
+
     
 end
